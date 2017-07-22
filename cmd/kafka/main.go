4197370115d7b690ad6e1263e6e0997c6080ace4
Christian Kniep <christian@qnib.org>
2017-07-22 11:54:40 +0200

    add cmd/kafka/main.go to bumpversion

--- cmd/kafka/main.go
+++ cmd/kafka/main.go
@@ -55,7 +55,7 @@ func main() {
 	app := cli.NewApp()
 	app.Name = "kafka example agent"
 	app.Usage = "kafka [options]"
-	app.Version = "0.0.0"
+	app.Version = "0.1.0"
 	app.Flags = []cli.Flag{
 		cli.StringFlag{
 			Name:  "config",
